772934a1c2697394b592f29f4e4bd45a586e08fa
Noah <1101qbtext@gmail.com>
2020-04-18 23:03:55 -0400

    Redesign of file finder is complete.

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 #include <ncurses.h>
 #include <string.h>
 #include <sys/stat.h>
+#include <linux/limits.h>
 
 #define true 1
 #define false 0
@@ -20,7 +21,7 @@ typedef enum
 typedef struct
 {
     Dir_Entry_Kind kind;
-    char name[256];
+    char name[NAME_MAX];
     int name_length;
 } Dir_Entry;
 
@@ -91,6 +92,321 @@ Const_Buffer const_buffer_new(void)
     return buffer;
 }
 
+#define BIT(n) (1 << n)
+
+typedef enum
+{
+    STATE_NONE,
+    STATE_LISTING = BIT(2),
+    STATE_NEW_FILE = BIT(3),
+} State;
+
+typedef enum
+{
+    MESSAGE_NONE,
+    MESSAGE_UPDATE_ENTRIES,
+    MESSAGE_CHANGE_DIRECTORY,
+    MESSAGE_CREATE_FILE,
+    MESSAGE_DELETE_FILE,
+} Message;
+
+typedef struct
+{
+    WINDOW *left;
+    int width;
+    int height;
+    WINDOW *new_file_box;
+    Message message;
+    char *message_change_directory_new_path;
+    Array entries;
+    State state;
+    int keep_running;
+    char current_directory_path[PATH_MAX];
+    DIR *current_directory;
+    int selection;
+    int show_hidden_files;
+    Const_Buffer input_buffer;
+} Context;
+
+void refresh_windows_that_need_it(Context *context)
+{
+    refresh();
+    wrefresh(context->left);
+    if (context->state & STATE_NEW_FILE)
+    {
+        wrefresh(context->new_file_box);
+    }
+}
+
+void clear_windows_that_need_it(Context *context)
+{
+    clear();
+    wclear(context->left);
+    if (context->state & STATE_NEW_FILE)
+    {
+        wclear(context->new_file_box);
+    }
+}
+
+void message_update_entries(Context *context)
+{
+    if (context->message != MESSAGE_DELETE_FILE)
+    {
+        context->selection = 0;
+    }
+
+    context->entries.count = 0;
+    for (struct dirent *ent = readdir(context->current_directory); ent;
+         ent = readdir(context->current_directory))
+    {
+        int add_entry = true;
+        if (ent->d_name[0] == '.')
+        {
+            add_entry = context->show_hidden_files;
+        }
+
+        if (add_entry)
+        {
+            Dir_Entry entry;
+            switch (ent->d_type)
+            {
+                case DT_REG:
+                entry.kind = DIR_ENTRY_FILE;
+                break;
+                
+                case DT_DIR:
+                entry.kind = DIR_ENTRY_DIRECTORY;
+                break;
+            }
+            entry.name_length = strlen(ent->d_name);
+            memcpy(entry.name, ent->d_name, entry.name_length);
+            entry.name[entry.name_length] = 0;
+            array_add(&context->entries, entry);
+        }
+    }
+    rewinddir(context->current_directory);
+}
+
+void message_create_file(Context *context)
+{
+    struct stat s;
+    int exist = stat(context->input_buffer.memory, &s);
+    if (exist == 0)
+    {
+        context->keep_running = false;
+        return;
+    }
+
+    FILE *file = fopen(context->input_buffer.memory, "w");
+    fclose(file);
+
+    memset(context->input_buffer.memory, 0, context->input_buffer.count);
+    context->input_buffer.count = 0;
+    message_update_entries(context);
+}
+
+void message_delete_file(Context *context)
+{
+    remove(context->entries.buffer[context->selection].name);
+    if (context->selection == context->entries.count - 1)
+    {
+        context->selection--;
+    }
+
+    message_update_entries(context);
+}
+
+void message_change_directory(Context *context)
+{
+    chdir(context->message_change_directory_new_path);
+    getcwd(context->current_directory_path, PATH_MAX);
+    closedir(context->current_directory);
+    context->current_directory = opendir(context->current_directory_path);
+    message_update_entries(context);
+}
+
+void draw_and_handle_input_for_new_file_box(Context *context)
+{
+    box(context->new_file_box, 0, 0);
+    char *message = "Name New File";
+    mvwprintw(context->new_file_box,
+              0,
+              (getmaxx(context->new_file_box) / 2) - (strlen(message) / 2),
+              "%s", message);
+    mvwprintw(context->new_file_box, 1, 1, "%s", context->input_buffer.memory);
+    int c = wgetch(context->new_file_box);
+    Const_Buffer *buffer = &context->input_buffer;
+    switch (c)
+    {
+        case 127:
+        if (buffer->count > 0)
+        {
+            buffer->count--;
+            buffer->memory[buffer->count] = 0;
+            wdelch(context->new_file_box);
+        }
+        break;
+
+        case '\n':
+        context->state &= ~STATE_NEW_FILE;
+        context->message = MESSAGE_CREATE_FILE;
+        break;
+
+        case 'q':
+        context->keep_running = false;
+        break;
+
+        default:
+        if (buffer->count < getmaxx(context->new_file_box) - 2)
+        {
+            buffer->memory[buffer->count] = c;
+            buffer->count++;
+        }
+        break;
+    }
+}
+
+void handle_input_for_listing(Context *context)
+{
+    int c = wgetch(context->left);
+    switch (c)
+    {
+        case 'q':
+        context->keep_running = false;
+        break;
+
+        case 'n':
+        context->state |= STATE_NEW_FILE;
+        context->new_file_box = newwin(3,
+                                       context->width / 2,
+                                       context->height / 4,
+                                       (context->width / 2) - (context->width / 4));
+        break;
+
+        case 'd':
+        context->message = MESSAGE_DELETE_FILE;
+        break;
+
+        case 's':
+        context->show_hidden_files = !context->show_hidden_files;
+        context->message = MESSAGE_UPDATE_ENTRIES;
+        break;
+
+        case 'b':
+        context->message = MESSAGE_CHANGE_DIRECTORY;
+        context->message_change_directory_new_path = "..";
+        break;
+
+        case 'e':
+        if (context->entries.buffer[context->selection].kind == DIR_ENTRY_DIRECTORY)
+        {
+            context->message = MESSAGE_CHANGE_DIRECTORY;
+            context->message_change_directory_new_path = context->entries.buffer[context->selection].name;
+        }
+        break;
+
+        case 'h':
+        if (context->selection > 0)
+        {
+            context->selection--;
+        }
+        break;
+
+        case 'l':
+        if (context->selection < context->entries.count - 1)
+        {
+            context->selection++;
+        }
+        break;
+    }
+}
+
+void draw_listing(Context *context)
+{
+    switch (context->message)
+    {
+        case MESSAGE_UPDATE_ENTRIES:
+        message_update_entries(context);
+        break;
+
+        case MESSAGE_CREATE_FILE:
+        message_create_file(context);
+        break;
+
+        case MESSAGE_DELETE_FILE:
+        message_delete_file(context);
+        break;
+
+        case MESSAGE_CHANGE_DIRECTORY:
+        message_change_directory(context);
+        break;
+
+        default:
+        break;
+    }
+
+    context->message = MESSAGE_NONE;
+
+    clear_windows_that_need_it(context);
+
+    mvprintw(0, 1, "%s", context->current_directory_path);
+
+    int entry_x = 2;
+    int entry_y = 2;
+
+    int i = 0;
+
+    context->entries.position = 0;
+    for_each_array (context->entries)
+    {
+        if (entry_x + it->name_length > context->width)
+        {
+            entry_y += 2;
+            entry_x = 2;
+        }
+
+        if (it->kind == DIR_ENTRY_DIRECTORY)
+        {
+            wattron(context->left, A_UNDERLINE);
+        }
+
+        mvwprintw(context->left,
+                  entry_y, entry_x,
+                  "%s", it->name);
+
+        wattroff(context->left, A_UNDERLINE);
+
+        if (i == context->selection)
+        {
+            mvwprintw(context->left,
+                      entry_y + 1, entry_x, "^");
+        }
+
+        entry_x += it->name_length;
+        entry_x++;
+
+        i++;
+    }
+
+    box(context->left, 0, 0);
+
+    refresh_windows_that_need_it(context);
+
+    if (context->keep_running)
+    {
+        int someone_else_is_getting_input = context->state & STATE_NEW_FILE;
+        if (someone_else_is_getting_input)
+        {
+            draw_and_handle_input_for_new_file_box(context);
+        }
+
+        if (!someone_else_is_getting_input)
+        {
+            handle_input_for_listing(context);
+        }
+    }
+}
+
 int main(void)
 {
     initscr();
@@ -99,300 +415,31 @@ int main(void)
     noecho();
     curs_set(0);
     
-    char cwd_buffer[512];
-    getcwd(cwd_buffer, 512);
-    DIR *current_directory = opendir(cwd_buffer);
-    
-    int initial_window_height = getmaxy(stdscr);
-    int initial_window_width = getmaxx(stdscr);
-    
-    WINDOW *left = newwin(initial_window_height - 1, initial_window_width, 1, 0);
+    Context context;
+    context.width = getmaxx(stdscr);
+    context.height = getmaxy(stdscr);
+    context.left = newwin(context.height - 1, context.width, 1, 0);
     // Later on we will allow two windows at a time to be rendered
     // WINDOW *right;
     
-    int entry_x = 2;
-    int entry_y = 2;
+    getcwd(context.current_directory_path, PATH_MAX);
+    context.current_directory = opendir(context.current_directory_path);
     
-    int update_directory_entries = true;
-    int show_hidden_files = false;
-    int selection = 0;
+    context.selection = 0;
+    context.show_hidden_files = false;
+    context.input_buffer = const_buffer_new();
+    context.new_file_box = NULL;
+    context.state = STATE_LISTING;
+    context.message = MESSAGE_UPDATE_ENTRIES;
+    context.entries = array_new();
 
-    int draw_new_file_dialog = false;
-    int create_new_file = false;
-    WINDOW *new_file_dialog = NULL;
-    Const_Buffer new_file_buffer = const_buffer_new();
-    
-    Array entries = array_new();
-    
-    int keep_running = true;
-    while (keep_running)
+    context.keep_running = true;
+    while(context.keep_running)
     {
-        int height;
-        int width;
-        // For every iteration of the loop
-        // make sure to grab the dimensions of the window
-        // This can change because the user could have resized the terminal
-        height = getmaxy(stdscr);
-        width = getmaxx(stdscr);
-        wresize(left, height - 1, width);
-
-        wclear(left);
-        if (draw_new_file_dialog)
-        {
-            wclear(new_file_dialog);
-        }
-        clear();
-        
-        box(left, 0, 0);
-        mvprintw(0, 1, "%s", cwd_buffer);
-        
-        // When the user presses 'enter' on the new file dialog
-        // box then actually create the file
-        if (create_new_file)
-        {
-            struct stat s;
-            int exist = stat(new_file_buffer.memory, &s);
-            if (exist == 0)
-            {
-                // Just exit for now if a the file we want to create
-                // already exists.
-                keep_running = false;
-                continue;
-            }
-
-            FILE *file = fopen(new_file_buffer.memory, "w");
-            fclose(file);
-
-            memset(new_file_buffer.memory, 0, new_file_buffer.count);
-            new_file_buffer.count = 0;
-            create_new_file = false;
-            selection = 0;
-            rewinddir(current_directory);
-            update_directory_entries = true;
-        }
-
-        if (update_directory_entries)
-        {
-            entries.count = 0;
-        }
-        
-        // Only update the entries array when we need to
-        // like if the current directory changes or
-        // if we need to reiterate over the directory
-        // again if hidden files are toggled or a file is created or deleted
-        entries.position = 0;
-        for (struct dirent *ent = readdir(current_directory);
-             update_directory_entries && ent;
-             ent = readdir(current_directory))
-        {
-            int add_entry = true;
-            if (ent->d_name[0] == '.')
-            {
-                add_entry = show_hidden_files;
-            }
-            
-            if (add_entry)
-            {
-                Dir_Entry entry;
-                entry.name_length = strlen(ent->d_name);
-                memcpy(entry.name, ent->d_name, entry.name_length);
-                entry.name[entry.name_length] = 0;
-                
-                switch (ent->d_type)
-                {
-                    case DT_REG:
-                    entry.kind = DIR_ENTRY_FILE;
-                    break;
-                    
-                    case DT_DIR:
-                    entry.kind = DIR_ENTRY_DIRECTORY;
-                    break;
-                }
-                
-                array_add(&entries, entry);
-            }
-        }
-        
-        update_directory_entries = false;
-        
-        // Display the contents of the directory
-        int i = 0;
-        for_each_array (entries)
-        {
-            // If the next item is too big to fit onto the current
-            // row then start rendering the entries two rows down
-            if (entry_x + it->name_length > width)
-            {
-                entry_y += 2;
-                entry_x = 1;
-            }
-            
-            if (it->kind == DIR_ENTRY_DIRECTORY)
-            {
-                wattron(left, A_UNDERLINE);
-            }
-            
-            mvwprintw(left, entry_y, entry_x, it->name);
-            
-            wattroff(left, A_UNDERLINE);
-            
-            if (selection == i)
-            {
-                mvwprintw(left, entry_y + 1, entry_x, "^");
-            }
-            
-            entry_x += it->name_length;
-            entry_x++; // For the space between entries
-            
-            i++;
-        }
-
-        // @Bug
-        // The new file dialog does not resize itself
-        // when the dimensions of the terminal changes
-        if (draw_new_file_dialog)
-        {
-            box(new_file_dialog, 0, 0);
-            char *message = "Name new file";
-            int dialog_width = getmaxx(new_file_dialog);
-            mvwprintw(new_file_dialog, 0, (dialog_width / 2) - strlen(message) / 2, "%s", message);
-            mvwprintw(new_file_dialog, 1, 1, "%s", new_file_buffer.memory);
-        }
-        
-        entry_y = 2;
-        entry_x = 2;
-        
-        refresh();
-        wrefresh(left);
-        if (draw_new_file_dialog)
-        {
-            wrefresh(new_file_dialog);
-        }
-        
-        // Get input from the standard screen
-        // if the new file dialog is not up
-        // and get input from the dialog if it is being rendered
-        int c;
-        if (!draw_new_file_dialog)
-        {
-            c = getch();
-            switch (c)
-            {
-                case 'q':
-                keep_running = false;
-                break;
-                
-                case 'b':
-                {
-                    chdir("..");
-                    closedir(current_directory);
-                    getcwd(cwd_buffer, 512);
-                    current_directory = opendir(cwd_buffer);
-                    update_directory_entries = true;
-                    selection = 0;
-                }
-                break;
-                
-                case 'e':
-                {
-                    Dir_Entry *entry = &entries.buffer[selection];
-                    if (entry->kind == DIR_ENTRY_DIRECTORY)
-                    {
-                        chdir(entry->name);
-                        closedir(current_directory);
-                        getcwd(cwd_buffer, 512);
-                        current_directory = opendir(cwd_buffer);
-                        update_directory_entries = true;
-                        selection = 0;
-                    }
-                }
-                break;
-                
-                case 's':
-                {
-                    show_hidden_files = !show_hidden_files;
-                    rewinddir(current_directory);
-                    update_directory_entries = true;
-                }
-                break;
-
-                case 'n':
-                {
-                    new_file_dialog = newwin(3, width / 2, 10, (width / 2) - (width / 4));
-                    draw_new_file_dialog = true;
-                }
-                break;
-
-                case 'd':
-                {
-                    // @Bug
-                    // This might fail becase we may not have privileges
-                    // to delete this file
-                    remove(entries.buffer[selection].name);
-                    rewinddir(current_directory);
-                    selection = 0;
-                    update_directory_entries = true;
-                }
-                break;
-                
-                case 'h':
-                {
-                    if (selection > 0)
-                    {
-                        selection--;
-                    }
-                }
-                break;
-                
-                case 'l':
-                {
-                    if (selection < entries.count - 1)
-                    {
-                        selection++;
-                    }
-                }
-                break;
-            }
-        } else
-        {
-            c = wgetch(new_file_dialog);
-            switch(c)
-            {
-                case KEY_ENTER:
-                case '\n':
-                {
-                    draw_new_file_dialog = false;
-                    delwin(new_file_dialog);
-                    create_new_file = true;
-                }
-                break;
-
-                case KEY_BACKSPACE:
-                case 127:
-                {
-
-                    if (new_file_buffer.count > 0)
-                    {
-                        new_file_buffer.count--;
-                        new_file_buffer.memory[new_file_buffer.count] = 0;
-                        wdelch(new_file_dialog);
-                    }
-                }
-                break;
-
-                default:
-                {
-                    if (new_file_buffer.count < 128)
-                    {
-                        new_file_buffer.memory[new_file_buffer.count] = c;
-                        new_file_buffer.count++;
-                    }
-                }
-            }
-        }
+        draw_listing(&context);
     }
     
-    closedir(current_directory);
+    closedir(context.current_directory);
     
     endwin();
 }
